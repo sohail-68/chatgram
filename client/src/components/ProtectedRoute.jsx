0c27018c48b5361bb59b34b8a8e8b1137cd12c86
Sohail khan <120656530+sohail-68@users.noreply.github.com>
2025-04-08 12:12:40 +0530

    Update ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ isAuthenticated, children }) => {
   const token = sessionStorage.getItem("token"); // सही तरीका
-  if (!token) {
+  if (token) {
     return <Navigate to="/login" replace />;
   }
   return children;
