50888d3af48effa5995a2c94a8adf64c99ab874b
Sohail khan <120656530+sohail-68@users.noreply.github.com>
2025-04-18 15:04:47 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -20,9 +20,9 @@ function SignUp() {
       // Assuming user data is returned after successful sign-up
       const userData = response.data.payload.user;
 
-      sessionStorage.setItem('user', JSON.stringify(userData));
-      sessionStorage.setItem('userid', userData.id);
-      sessionStorage.setItem('token', response.data.token);
+      localStorage.setItem('user', JSON.stringify(userData));
+      localStorage.setItem('userid', userData.id);
+      localStorage.setItem('token', response.data.token);
 
       navigate('/login');
     } catch (error) {
